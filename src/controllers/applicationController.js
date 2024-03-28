eba2fef921c2d003759259b516004c4010459312
Shakil Ahamed <shakil.ahamed.tousif.800@gmail.com>
2024-03-28 17:37:56 +0600

    Add request model and update getAppByUser to include requests

--- src/controllers/applicationController.js
+++ src/controllers/applicationController.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
 const User = require("../models/user");
 const Application = require("../models/application");
+const Request = require("../models/request");
 
 const createApp = async (req, res) => {
     try {
@@ -72,6 +73,7 @@ const deleteApp = async (req, res) => {
         res.status(500).json({ message: 'Server Side Error', data: false });
     }
 }
+
 const updateApp = async (req, res) => {
     try {
 
@@ -137,11 +139,33 @@ const getAppByUser = async (req, res) => {
 
         const user = await User.findOne({ _uuid: auth_uuid });
 
-        const application = await Application.find({ user_id: user?._id });
+        // Use aggregation to fetch applications and their requests
+        const applicationsWithRequests = await Application.aggregate([
+            // Match applications by user_id
+            { $match: { user_id: user._id.toString() } },
+            // Lookup to join with requests
+            {
+                $lookup: {
+                    from: "requests", // The collection to join
+                    localField: "_id", // Field from the applications collection
+                    foreignField: "application", // Field from the requests collection
+                    as: "requests" // The array to populate with the matched documents
+                },
+
+            },
+            {
+                $project: {
+                    name: 1, // Assuming you want to keep the application's name
+                    domain: 1, // And its domain
+                    requestCount: { $size: "$requests" } // Count the number of requests
+                }
+            }
+        ]);
+
 
 
         // Return the user ID
-        res.json({ message: "Application By User ID", application });
+        res.json({ message: "Application By User ID", application: applicationsWithRequests });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server Side Error', data: false });
